dd3d76071ddc6331f98cdd815f28bf89616cabf3
Alex Pimenta <aspepper@hotmail.com>
2025-10-15 20:31:29 -0300

    Fix forgot password keyboard options dependency

--- PerueiroApp/app/src/main/java/com/idealinspecao/perueiroapp/ui/screens/login/ForgotPasswordScreen.kt
+++ PerueiroApp/app/src/main/java/com/idealinspecao/perueiroapp/ui/screens/login/ForgotPasswordScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.idealinspecao.perueiroapp.data.remote.PasswordResetResult
